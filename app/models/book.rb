d0506f8676cfebdd9a37acf473f2efd487c6ea0b
Demaris Ashmore <demiashmore@yahoo.com>
2023-03-02 15:49:26 -0700

    Destroy associated reviews with deletion of book

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,7 +2,7 @@ class Book < ApplicationRecord
 
     validates :image_url, :description, :title, :author, presence: true
 
-    has_many :reviews
+    has_many :reviews, dependent: :destroy
     has_many :users, through: :reviews
 
 end
