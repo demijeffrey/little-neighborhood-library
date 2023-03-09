9e29ca7acfbd09f9fcdb7702c73f48250b5794a7
Demaris Ashmore <demiashmore@yahoo.com>
2023-03-09 14:47:07 -0700

    Use NavLink in navbar

--- client/src/Components/NavBar.js
+++ client/src/Components/NavBar.js
@@ -1,6 +1,6 @@
 import { useContext } from "react"
 import { UserContext } from "../context/user"
-import { useNavigate } from "react-router-dom"
+import { NavLink, useNavigate } from "react-router-dom"
 import '../App.css';
 
 function NavBar() {
@@ -18,19 +18,53 @@ function NavBar() {
         })
     }
 
+    // if(!loggedIn) {
+    //     return(
+    //         <div className="title" data-bs-theme="dark">
+    //             <h3>Little Neighborhood Library</h3>
+    //             <ul className="nav justify-content-end bg-dark">
+    //                 <li className="nav-item">
+    //                     <a className="nav-link active" aria-current="page" href="/">Home</a>
+    //                 </li>
+    //                 <li className="nav-item">
+    //                     <a className="nav-link" href="/signup">Signup</a>
+    //                 </li>
+    //                 <li className="nav-item">
+    //                     <a className="nav-link" href="/login">Login</a>
+    //                 </li>
+    //             </ul>
+    //         </div>
+    //     )
+    // } else {
+    //     return(
+    //         <div className="title" data-bs-theme="dark">
+    //             <h3>Little Neighborhood Library</h3>
+    //             <ul className="nav justify-content-end">
+    //                 <li className="nav-item">
+    //                     <a className="nav-link active" aria-current="page" href="/">My Home</a>
+    //                 </li>
+    //                 <li className="nav-item">
+    //                     <a className="nav-link" href="/books">Books</a>
+    //                 </li>
+    //                 <button onClick={logoutUser}>Logout</button>
+    //             </ul>
+    //         </div>
+    //     )
+    // }
+
     if(!loggedIn) {
         return(
             <div className="title" data-bs-theme="dark">
                 <h3>Little Neighborhood Library</h3>
                 <ul className="nav justify-content-end bg-dark">
                     <li className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="/">Home</a>
+                        <NavLink className="nav-link active" aria-current="page" to="/">Home</NavLink>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="/signup">Signup</a>
+                        <NavLink className="nav-link" to="/signup">Signup</NavLink>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="/login">Login</a>
+                        <NavLink className="nav-link" to="/login">Login</NavLink>
                     </li>
                 </ul>
             </div>
@@ -41,10 +75,10 @@ function NavBar() {
                 <h3>Little Neighborhood Library</h3>
                 <ul className="nav justify-content-end">
                     <li className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="/">My Home</a>
+                        <NavLink className="nav-link active" aria-current="page" to="/">My Home</NavLink>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="/books">Books</a>
+                        <NavLink className="nav-link" to="/books">Books</NavLink>
                     </li>
                     <button onClick={logoutUser}>Logout</button>
                 </ul>
